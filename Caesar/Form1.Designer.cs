7c3dc98ac51bf4e135206b43bae6ca2a258adc74
PodmazowS <podmazowstas@gmail.com>
2022-12-04 23:14:12 +0100

    Add project files.

---
+++ Caesar/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Caesar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputText = new System.Windows.Forms.TextBox();
+            this.OutputText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.input_key = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputText
+            // 
+            this.InputText.Location = new System.Drawing.Point(51, 99);
+            this.InputText.Multiline = true;
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(200, 100);
+            this.InputText.TabIndex = 0;
+            // 
+            // OutputText
+            // 
+            this.OutputText.Location = new System.Drawing.Point(493, 99);
+            this.OutputText.Multiline = true;
+            this.OutputText.Name = "OutputText";
+            this.OutputText.Size = new System.Drawing.Size(200, 100);
+            this.OutputText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Normalny tekst";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(493, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Zaszyfrowany tekst";
+            // 
+            // input_key
+            // 
+            this.input_key.Location = new System.Drawing.Point(301, 115);
+            this.input_key.Name = "input_key";
+            this.input_key.Size = new System.Drawing.Size(125, 27);
+            this.input_key.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Szyfruj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Oczysc";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(316, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Decrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ilość kroków:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 353);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.input_key);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputText);
+            this.Controls.Add(this.OutputText);
+            this.Name = "Form1";
+            this.Text = "Caesar cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox InputText;
+        private TextBox OutputText;
+        private Label label1;
+        private Label label2;
+        private TextBox input_key;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label3;
+    }
+}
---
+++ Caesar/Form1.cs
@@ -0,0 +1,169 @@
+﻿namespace Caesar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int key = Convert.ToInt32(input_key.Text);
+                string str_text = InputText.Text;
+                string out_text = null;
+                char[] AlphabetUpper = {'A','Ą','B','C','Ć','D','E','Ę','F','G','H','I','J','K','L','Ł','M','N','Ń','O',
+                                        'Ó','P','Q','R','S','Ś','T','U','V', 'W','X','Y','Z','Ź','Ż' };
+
+                char[] AlphabetLower ={'a','ą','b','c','ć','d','е','ę','f','g','h','i','j','k','l','ł','m','n','ń','o',
+                                        'ó','p','q','r','s','ś','t','u','v','w','x','y','z','ź','ż'};
+
+
+                char[] array_text = str_text.ToCharArray();
+                for (int i = 0; i < array_text.Length; i++)
+                {
+
+                    if (array_text[i] == '!' || array_text[i] == ' ' || array_text[i] == '.' || array_text[i] == ',') out_text = out_text + array_text[i];
+
+
+                    if (char.IsUpper(array_text[i]))
+                    {
+
+                        for (int j = 0; j < AlphabetUpper.Length; j++)
+
+
+                            if (array_text[i] == AlphabetUpper[j])
+                            {
+
+                                try
+                                {
+                                    out_text = out_text + (char)AlphabetUpper[j + key];
+                                }
+
+                                catch
+                                {
+                                    out_text = out_text + (char)AlphabetUpper[j + key - 35];
+                                }
+                            }
+                    }
+
+
+                    if (char.IsLower(array_text[i]))
+                    {
+                        for (int j = 0; j < AlphabetLower.Length; j++)
+                            if (array_text[i] == AlphabetLower[j])
+                            {
+                                try
+                                {
+                                    out_text = out_text + (char)AlphabetLower[j + key];
+                                }
+                                catch
+                                {
+                                    out_text = out_text + (char)AlphabetLower[j + key - 35];
+                                }
+                            }
+                    }
+                }
+
+
+
+
+                OutputText.Text = out_text;
+            }
+
+
+            catch
+            {
+                OutputText.Text = "Proszę zapełnić wszystkie pola!";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+              InputText.Text = "";
+              OutputText.Text = "";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string arrStr = OutputText.Text;
+
+            int max = 0; // największa liczba wystąpień danej litery to 1
+            int max2 = 0; // liczba wystąpień określonej litery w jednym przejściu przez cykl
+
+            char letter = 'a'; // najczęstszą literą jest 1
+            char letter2; // najczęstszą literą jest 2
+
+            // wyszukuje literę, która występuje najczęściej w tekście
+            for (int j = 0; j < arrStr.Length; j++)
+            {
+                letter2 = arrStr[j];
+                for (int i = 0; i < arrStr.Length; i++)
+                {
+                    if (Char.IsLetter(arrStr[i]) && arrStr[i] == letter2)
+                        max2 += 1;
+                }
+                if (max2 > max)
+                {
+                    max = max2;
+                    letter = letter2;
+                }
+                max2 = 0;
+            }
+
+            // alphabet (35 liter)
+            string arrAbc = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż";
+
+            int c1 = arrAbc.IndexOf('a'); // pozycja litery a, która występuje najczęściej
+            int c2 = 0; // indeks alfabetyczny najczęściej występującego elementu
+
+            // wyszukaj indeks w alfabecie litery, która występuje najczęściej
+            for (int i = 0; i < arrAbc.Length; i++)
+            {
+                if (arrAbc[i] == letter)
+                {
+                    c2 = i;
+                }
+            }
+
+            // obliczenie wartości przemieszczenia
+            int sh = (c1 - c2) % 35;
+            if (sh < 0)
+            {
+                sh = sh * (-1);
+            }
+
+            string res = "";
+
+            // dekodowanie zaszyfrowanego tekstu ze znanym krokiem
+            for (int i = 0; i < arrStr.Length; i++)
+            {
+                if (!Char.IsLetter(arrStr[i]))
+                {
+                    res += arrStr[i];
+                }
+                else
+                {
+                    int position = arrAbc.IndexOf(arrStr[i]) - sh;
+                    if (position < 0)
+                    {
+                        position += arrAbc.Length;
+                    }
+                    res += arrAbc[position];
+                }
+            }
+            InputText.Text = res;
+
+        }
+    }
+    }
